4d810a2d220e91e02008e2b4af163763b04ab5f9
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2025-02-20 23:10:50 +0300

    update new godbtest_21mb_of6a

--- db_postgresql.go
+++ db_postgresql.go
@@ -11,7 +11,7 @@ var db *sql.DB
 // Инициализация базы данных
 func InitDB() {
 	var err error
-	connStr := "postgresql://godbtest_user:lUDEQDsf2MrpRu80RajTBSOG70RNBcY4@dpg-cu74g1q3esus73fg1beg-a.oregon-postgres.render.com/godbtest_21mb"
+	connStr := "postgresql://godbtest_user:944sBzadpIXTdF9yGaxg5TFTQyTeaWxY@dpg-curoet52ng1s73dkune0-a.oregon-postgres.render.com/godbtest_21mb_of6a"
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
