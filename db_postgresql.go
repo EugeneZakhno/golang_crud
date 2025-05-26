2fc16b7a52a5c8a59d8c9af4c99479e15d2d108e
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2025-05-26 15:31:30 +0300

    changed DB credentials

--- db_postgresql.go
+++ db_postgresql.go
@@ -11,7 +11,7 @@ var db *sql.DB
 // Инициализация базы данных
 func InitDB() {
 	var err error
-	connStr := "postgresql://godbtest_user:eC2t9gs2B5dqydPl8oev3Luju4GXucgB@dpg-d059m1p5pdvs73etokbg-a.oregon-postgres.render.com/godbtest_1wll"
+	connStr := "postgresql://godbtest_8g85_user:gHNGuGjrVDcjlM9TdIDm8nTFFguj1QnU@dpg-d0q4hrvdiees738o58d0-a.oregon-postgres.render.com/godbtest_8g85"
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err)
